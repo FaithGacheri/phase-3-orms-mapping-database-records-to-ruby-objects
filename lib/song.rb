c51157584544aa06879368efb5ae4f805a5c7b00
“[firstname <faith.gacheri@student.moringaschool.com>
2022-11-01 09:34:39 +0300

    Completed

--- lib/song.rb
+++ lib/song.rb
@@ -34,6 +34,15 @@ class Song
       VALUES (?, ?)
     SQL
 
+  #   sql = <<-SQL
+  #     SELECT *
+  #     FROM songs
+  #  SQL
+
+  #  DB[:conn].execute(sql).map do |row|
+  #   self.new_from_db(row)
+  
+  # end
     # insert the song
     DB[:conn].execute(sql, self.name, self.album)
 
@@ -49,4 +58,31 @@ class Song
     song.save
   end
 
+  def self.new_from_db(row)
+    # self.new is equivalent to Song.new
+    self.new(id: row[0], name: row[1], album: row[2])
+  end
+  def self.all
+    sql = <<-SQL
+      SELECT *
+      FROM songs
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM songs
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
 end
